cbe7d9fb7b6f09800267b4c81b6e8200e279992d
hkbnstanleylee <110223397+hkbnstanleylee@users.noreply.github.com>
2022-10-06 16:25:08 +0800

    Update aws-cli-create-vpc.sh
    forstanleytesting

--- aws-cli-create-vpc.sh
+++ aws-cli-create-vpc.sh
@@ -5,7 +5,7 @@
 #
 # SYNOPSIS
 #    Automates the creation of a custom IPv4 VPC, having both a public and a
-#    private subnet, and a NAT gateway.
+#    private subnet, and a Internet gateway.
 #
 # DESCRIPTION
 #    This shell script leverages the AWS Command Line Interface (AWS CLI) to
@@ -15,27 +15,27 @@
 #==============================================================================
 #
 # NOTES
-#   VERSION:   0.1.0
-#   LASTEDIT:  03/18/2017
-#   AUTHOR:    Joe Arauzo
-#   EMAIL:     joe@arauzo.net
-#   REVISIONS:
-#       0.1.0  03/18/2017 - first release
-#       0.0.1  02/25/2017 - work in progress
 #
 #==============================================================================
 #   MODIFY THE SETTINGS BELOW
 #==============================================================================
 #
-AWS_REGION="us-west-1"
-VPC_NAME="My VPC"
-VPC_CIDR="10.0.0.0/16"
-SUBNET_PUBLIC_CIDR="10.0.1.0/24"
-SUBNET_PUBLIC_AZ="us-west-1a"
-SUBNET_PUBLIC_NAME="10.0.1.0 - us-west-1a"
-SUBNET_PRIVATE_CIDR="10.0.2.0/24"
-SUBNET_PRIVATE_AZ="us-west-1c"
-SUBNET_PRIVATE_NAME="10.0.2.0 - us-west-1b"
+AWS_REGION="ap-east-1"
+VPC_NAME="testnetwork"
+VPC_CIDR="172.50.200.0/25"
+Environment_TAG="Production
+SUBNET_PUBLIC1_CIDR="172.50.200.0/27"
+SUBNET_PUBLIC1_AZ="ap-southeast-1a"
+SUBNET_PUBLIC1_NAME="testnetwork_PUB_1"
+SUBNET_PUBLIC2_CIDR="172.50.200.32/27"
+SUBNET_PUBLIC2_AZ="ap-southeast-1b"
+SUBNET_PUBLIC2_NAME="testnetwork_PUB_2"
+SUBNET_PRIVATE1_CIDR="172.50.200.64/27"
+SUBNET_PRIVATE1_AZ="ap-southeast-1a"
+SUBNET_PRIVATE1_NAME="testnetwork_PRV_1"
+SUBNET_PRIVATE2_CIDR="172.50.200.96/27"
+SUBNET_PRIVATE2_AZ="ap-southeast-1b"
+SUBNET_PRIVATE2_NAME="testnetwork_PRV_2"
 CHECK_FREQUENCY=5
 #
 #==============================================================================
@@ -54,48 +54,87 @@ echo "  VPC ID '$VPC_ID' CREATED in '$AWS_REGION' region."
 # Add Name tag to VPC
 aws ec2 create-tags \
   --resources $VPC_ID \
-  --tags "Key=Name,Value=$VPC_NAME" \
+  --tags Key=Environment,Value=$Environment_TAG \
+  --tags Key=Name,Value=$VPC_NAME \
   --region $AWS_REGION
 echo "  VPC ID '$VPC_ID' NAMED as '$VPC_NAME'."
 
-# Create Public Subnet
+#60 Create Public1 Subnet
 echo "Creating Public Subnet..."
-SUBNET_PUBLIC_ID=$(aws ec2 create-subnet \
+SUBNET_PUBLIC1_ID=$(aws ec2 create-subnet \
   --vpc-id $VPC_ID \
-  --cidr-block $SUBNET_PUBLIC_CIDR \
-  --availability-zone $SUBNET_PUBLIC_AZ \
+  --cidr-block $SUBNET_PUBLIC1_CIDR \
+  --availability-zone $SUBNET_PUBLIC1_AZ \
   --query 'Subnet.{SubnetId:SubnetId}' \
   --output text \
   --region $AWS_REGION)
-echo "  Subnet ID '$SUBNET_PUBLIC_ID' CREATED in '$SUBNET_PUBLIC_AZ'" \
+echo "  Subnet ID '$SUBNET_PUBLIC1_ID' CREATED in '$SUBNET_PUBLIC1_AZ'" \
   "Availability Zone."
 
-# Add Name tag to Public Subnet
+# Add Name tag to Public1 Subnet
 aws ec2 create-tags \
-  --resources $SUBNET_PUBLIC_ID \
-  --tags "Key=Name,Value=$SUBNET_PUBLIC_NAME" \
+  --resources $SUBNET_PUBLIC1_ID \
+  --tags "Key=Name,Value=$SUBNET_PUBLIC1_NAME" \
   --region $AWS_REGION
-echo "  Subnet ID '$SUBNET_PUBLIC_ID' NAMED as" \
-  "'$SUBNET_PUBLIC_NAME'."
+echo "  Subnet ID '$SUBNET_PUBLIC1_ID' NAMED as" \
+  "'$SUBNET_PUBLIC1_NAME'."
 
-# Create Private Subnet
-echo "Creating Private Subnet..."
-SUBNET_PRIVATE_ID=$(aws ec2 create-subnet \
+echo "Creating Public2 Subnet..."
+SUBNET_PUBLIC2_ID=$(aws ec2 create-subnet \
   --vpc-id $VPC_ID \
-  --cidr-block $SUBNET_PRIVATE_CIDR \
-  --availability-zone $SUBNET_PRIVATE_AZ \
+  --cidr-block $SUBNET_PUBLIC2_CIDR \
+  --availability-zone $SUBNET_PUBLIC2_AZ \
   --query 'Subnet.{SubnetId:SubnetId}' \
   --output text \
   --region $AWS_REGION)
-echo "  Subnet ID '$SUBNET_PRIVATE_ID' CREATED in '$SUBNET_PRIVATE_AZ'" \
+echo "  Subnet ID '$SUBNET_PUBLIC2_ID' CREATED in '$SUBNET_PUBLIC2_AZ'" \
   "Availability Zone."
 
-# Add Name tag to Private Subnet
+# Add Name tag to Public2 Subnet
 aws ec2 create-tags \
-  --resources $SUBNET_PRIVATE_ID \
-  --tags "Key=Name,Value=$SUBNET_PRIVATE_NAME" \
+  --resources $SUBNET_PUBLIC2_ID \
+  --tags "Key=Name,Value=$SUBNET_PUBLIC2_NAME" \
   --region $AWS_REGION
-echo "  Subnet ID '$SUBNET_PRIVATE_ID' NAMED as '$SUBNET_PRIVATE_NAME'."
+echo "  Subnet ID '$SUBNET_PUBLIC2_ID' NAMED as" \
+  "'$SUBNET_PUBLIC2_NAME'."
+
+#99 Create Private1 Subnet
+echo "Creating Private1 Subnet..."
+SUBNET_PRIVATE1_ID=$(aws ec2 create-subnet \
+  --vpc-id $VPC_ID \
+  --cidr-block $SUBNET_PRIVATE1_CIDR \
+  --availability-zone $SUBNET_PRIVATE1_AZ \
+  --query 'Subnet.{SubnetId:SubnetId}' \
+  --output text \
+  --region $AWS_REGION)
+echo "  Subnet ID '$SUBNET_PRIVATE1_ID' CREATED in '$SUBNET_PRIVATE1_AZ'" \
+  "Availability Zone."
+
+# Add Name tag to Private1 Subnet
+aws ec2 create-tags \
+  --resources $SUBNET_PRIVATE1_ID \
+  --tags "Key=Name,Value=$SUBNET_PRIVATE1_NAME" \
+  --region $AWS_REGION
+echo "  Subnet ID '$SUBNET_PRIVATE1_ID' NAMED as '$SUBNET_PRIVATE1_NAME'."
+
+# Create Private2 Subnet
+echo "Creating Private2 Subnet..."
+SUBNET_PRIVATE2_ID=$(aws ec2 create-subnet \
+  --vpc-id $VPC_ID \
+  --cidr-block $SUBNET_PRIVATE2_CIDR \
+  --availability-zone $SUBNET_PRIVATE2_AZ \
+  --query 'Subnet.{SubnetId:SubnetId}' \
+  --output text \
+  --region $AWS_REGION)
+echo "  Subnet ID '$SUBNET_PRIVATE2_ID' CREATED in '$SUBNET_PRIVATE2_AZ'" \
+  "Availability Zone."
+
+# Add Name tag to Private2 Subnet
+aws ec2 create-tags \
+  --resources $SUBNET_PRIVATE2_ID \
+  --tags "Key=Name,Value=$SUBNET_PRIVATE2_NAME" \
+  --region $AWS_REGION
+echo "  Subnet ID '$SUBNET_PRIVATE2_ID' NAMED as '$SUBNET_PRIVATE2_NAME'."
 
 # Create Internet gateway
 echo "Creating Internet Gateway..."
@@ -130,78 +169,52 @@ RESULT=$(aws ec2 create-route \
 echo "  Route to '0.0.0.0/0' via Internet Gateway ID '$IGW_ID' ADDED to" \
   "Route Table ID '$ROUTE_TABLE_ID'."
 
-# Associate Public Subnet with Route Table
+# Associate Public1 Subnet with Route Table
 RESULT=$(aws ec2 associate-route-table  \
-  --subnet-id $SUBNET_PUBLIC_ID \
+  --subnet-id $SUBNET_PUBLIC1_ID \
   --route-table-id $ROUTE_TABLE_ID \
   --region $AWS_REGION)
-echo "  Public Subnet ID '$SUBNET_PUBLIC_ID' ASSOCIATED with Route Table ID" \
+echo "  Public Subnet ID '$SUBNET_PUBLIC1_ID' ASSOCIATED with Route Table ID" \
   "'$ROUTE_TABLE_ID'."
 
-# Enable Auto-assign Public IP on Public Subnet
+# Associate Public2 Subnet with Route Table
+RESULT=$(aws ec2 associate-route-table  \
+  --subnet-id $SUBNET_PUBLIC2_ID \
+  --route-table-id $ROUTE_TABLE_ID \
+  --region $AWS_REGION)
+echo "  Public Subnet ID '$SUBNET_PUBLIC2_ID' ASSOCIATED with Route Table ID" \
+  "'$ROUTE_TABLE_ID'."
+
+# Associate Private1 Subnet with Route Table
+RESULT=$(aws ec2 associate-route-table  \
+  --subnet-id $SUBNET_PRIVATE1_ID \
+  --route-table-id $ROUTE_TABLE_ID \
+  --region $AWS_REGION)
+echo "  Private Subnet ID '$SUBNET_PRIVATE1_ID' ASSOCIATED with Route Table ID" \
+  "'$ROUTE_TABLE_ID'."
+
+# Associate Private2 Subnet with Route Table
+RESULT=$(aws ec2 associate-route-table  \
+  --subnet-id $SUBNET_PRIVATE2_ID \
+  --route-table-id $ROUTE_TABLE_ID \
+  --region $AWS_REGION)
+echo "  Private Subnet ID '$SUBNET_PRIVATE2_ID' ASSOCIATED with Route Table ID" \
+  "'$ROUTE_TABLE_ID'."
+
+# Enable Auto-assign Public IP on Public1 Subnet
 aws ec2 modify-subnet-attribute \
-  --subnet-id $SUBNET_PUBLIC_ID \
+  --subnet-id $SUBNET_PUBLIC1_ID \
   --map-public-ip-on-launch \
   --region $AWS_REGION
-echo "  'Auto-assign Public IP' ENABLED on Public Subnet ID" \
+echo "  'Auto-assign Public IP' ENABLED on Public1 Subnet ID" \
+  "'$SUBNET_PUBLIC1_ID'."
+
+# Enable Auto-assign Public IP on Public2 Subnet
+aws ec2 modify-subnet-attribute \
+  --subnet-id $SUBNET_PUBLIC2_ID \
+  --map-public-ip-on-launch \
+  --region $AWS_REGION
+echo "  'Auto-assign Public IP' ENABLED on Public2 Subnet ID" \
   "'$SUBNET_PUBLIC_ID'."
 
-# Allocate Elastic IP Address for NAT Gateway
-echo "Creating NAT Gateway..."
-EIP_ALLOC_ID=$(aws ec2 allocate-address \
-  --domain vpc \
-  --query '{AllocationId:AllocationId}' \
-  --output text \
-  --region $AWS_REGION)
-echo "  Elastic IP address ID '$EIP_ALLOC_ID' ALLOCATED."
-
-# Create NAT Gateway
-NAT_GW_ID=$(aws ec2 create-nat-gateway \
-  --subnet-id $SUBNET_PUBLIC_ID \
-  --allocation-id $EIP_ALLOC_ID \
-  --query 'NatGateway.{NatGatewayId:NatGatewayId}' \
-  --output text \
-  --region $AWS_REGION)
-FORMATTED_MSG="Creating NAT Gateway ID '$NAT_GW_ID' and waiting for it to "
-FORMATTED_MSG+="become available.\n    Please BE PATIENT as this can take some "
-FORMATTED_MSG+="time to complete.\n    ......\n"
-printf "  $FORMATTED_MSG"
-FORMATTED_MSG="STATUS: %s  -  %02dh:%02dm:%02ds elapsed while waiting for NAT "
-FORMATTED_MSG+="Gateway to become available..."
-SECONDS=0
-LAST_CHECK=0
-STATE='PENDING'
-until [[ $STATE == 'AVAILABLE' ]]; do
-  INTERVAL=$SECONDS-$LAST_CHECK
-  if [[ $INTERVAL -ge $CHECK_FREQUENCY ]]; then
-    STATE=$(aws ec2 describe-nat-gateways \
-      --nat-gateway-ids $NAT_GW_ID \
-      --query 'NatGateways[*].{State:State}' \
-      --output text \
-      --region $AWS_REGION)
-    STATE=$(echo $STATE | tr '[:lower:]' '[:upper:]')
-    LAST_CHECK=$SECONDS
-  fi
-  SECS=$SECONDS
-  STATUS_MSG=$(printf "$FORMATTED_MSG" \
-    $STATE $(($SECS/3600)) $(($SECS%3600/60)) $(($SECS%60)))
-  printf "    $STATUS_MSG\033[0K\r"
-  sleep 1
-done
-printf "\n    ......\n  NAT Gateway ID '$NAT_GW_ID' is now AVAILABLE.\n"
-
-# Create route to NAT Gateway
-MAIN_ROUTE_TABLE_ID=$(aws ec2 describe-route-tables \
-  --filters Name=vpc-id,Values=$VPC_ID Name=association.main,Values=true \
-  --query 'RouteTables[*].{RouteTableId:RouteTableId}' \
-  --output text \
-  --region $AWS_REGION)
-echo "  Main Route Table ID is '$MAIN_ROUTE_TABLE_ID'."
-RESULT=$(aws ec2 create-route \
-  --route-table-id $MAIN_ROUTE_TABLE_ID \
-  --destination-cidr-block 0.0.0.0/0 \
-  --gateway-id $NAT_GW_ID \
-  --region $AWS_REGION)
-echo "  Route to '0.0.0.0/0' via NAT Gateway with ID '$NAT_GW_ID' ADDED to" \
-  "Route Table ID '$MAIN_ROUTE_TABLE_ID'."
 echo "COMPLETED"
